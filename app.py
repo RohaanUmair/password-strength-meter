f0f0a7fbd93e206803c21f5ccbdffc056038c75c
RohaanUmair <m.rohaanumair@gmail.com>
2025-03-05 23:19:16 +0500

    used streamlit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import re
+import streamlit as st
 
 def check_password_strength(password):
     score = 0
@@ -7,34 +8,42 @@ def check_password_strength(password):
     if len(password) >= 8:
         score += 1
     else:
-        print("❌ Password should be at least 8 characters long.")
+        st.text("❌ Password should be at least 8 characters long.")
     
     # Upper & Lowercase Check
     if re.search(r"[A-Z]", password) and re.search(r"[a-z]", password):
         score += 1
     else:
-        print("❌ Include both uppercase and lowercase letters.")
+        st.text("❌ Include both uppercase and lowercase letters.")
     
     # Digit Check
     if re.search(r"\d", password):
         score += 1
     else:
-        print("❌ Add at least one number (0-9).")
+        st.text("❌ Add at least one number (0-9).")
     
     # Special Character Check
     if re.search(r"[!@#$%^&*]", password):
         score += 1
     else:
-        print("❌ Include at least one special character (!@#$%^&*).")
+        st.text("❌ Include at least one special character (!@#$%^&*).")
     
     # Strength Rating
     if score == 4:
-        print("✅ Strong Password!")
+        st.text("✅ Strong Password!")
     elif score == 3:
-        print("⚠️ Moderate Password - Consider adding more security features.")
+        st.text("⚠️ Moderate Password - Consider adding more security features.")
     else:
-        print("❌ Weak Password - Improve it using the suggestions above.")
+        st.text("❌ Weak Password - Improve it using the suggestions above.")
+
+
+# Title
+st.title('Password Strength Meter')
 
 # Get user input
-password = input("Enter your password: ")
-check_password_strength(password)
+password = st.text_input('Input Your Password to Check its Strength')
+
+
+# Check Password Strength
+if password:
+    check_password_strength(password)
